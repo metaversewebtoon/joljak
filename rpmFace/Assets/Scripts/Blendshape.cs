8353bff1d71b75d11d1d59333d6ed8b652c77ca8
ywe <youngwook4146@gmail.com>
2023-02-06 11:09:02 +0900

    Fix : 빈 폴더 문제 해결

---
+++ rpmFace/Assets/Scripts/Blendshape.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// Wrapper Class for Positive & Negative Values of Blendshapes
+/// </summary>
+namespace ywe.Blendshapes
+{
+    public class Blendshape
+    {
+        public int positiveIndex { get; set; }
+        //public int negativeIndex { get; set; }
+
+        //public Blendshape(int positiveIndex, int negativeIndex)
+        public Blendshape(int positiveIndex)
+        {
+            this.positiveIndex = positiveIndex;
+            //this.negativeIndex = negativeIndex;
+        }
+
+    } 
+}
