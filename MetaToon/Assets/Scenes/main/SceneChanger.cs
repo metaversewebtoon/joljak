e28b7452b2ca51577a73685414f37d062b0bc085
eunjin3481 <eunjin3481@gmail.com>
2023-02-06 10:50:16 +0900

    ui

---
+++ MetaToon/Assets/Scenes/main/SceneChanger.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // 클릭한 버튼 이름 추출 함수
+    public string ButtonName()
+    {
+        // 클릭한 버튼 오브젝트의 이름 저장
+        string objectName = EventSystem.current.currentSelectedGameObject.name;
+
+        print("버튼 클릭");
+        print("버튼 이름: " + objectName);
+
+        // 버튼 이름을 '_' 기준으로 자르기
+        int strIndex = objectName.LastIndexOf('_');
+        string buttonName = objectName.Substring(strIndex + 1);
+
+        return buttonName;
+    }
+
+
+    // 씬 변환 함수
+    public void SceneChange()
+    {
+        string sceneName = ButtonName();
+
+        // 씬 변환
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ MetaToon/Assets/Scenes/production/lookScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class lookScript : MonoBehaviour
+{
+
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void LookApply()
+    {
+        string buttonName = GameObject.Find("GameObject").GetComponent<SceneChanger>().ButtonName();
+        
+
+        animator.runtimeAnimatorController = (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load(buttonName, typeof(RuntimeAnimatorController)));
+    }
+
+
+}
