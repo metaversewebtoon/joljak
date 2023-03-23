fc0fa88c6e7691bbcf00f28f3d31d8635f10af33
Joogba <tjdwn0802@naver.com>
2023-03-23 18:40:18 +0900

    폴더 정리

---
+++ MetaToon/Assets/Render/CameraRender.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+
+public class CameraRender : MonoBehaviour
+{
+    // Choose a shader to use from inspector menu
+    public Shader replacementShader;
+
+    void Start()
+    {
+        Camera cam = GetComponent<Camera>();
+        // replacementShader : The shader to use for every game object captured through camera.
+        // "" : Render type tag(e.g. Opaque, Transparent). Empty by default.
+        cam.SetReplacementShader(replacementShader, "");
+    }
+}
---
+++ MetaToon/Assets/Scripts/production/prefabScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void prefabCreate()
+    {
+        string buttonName = GameObject.Find("GameObject").GetComponent<SceneChanger>().ButtonName(1);
+        string prefabName = buttonName + "_prefab";
+
+        GameObject obj = Instantiate(Resources.Load<GameObject>(prefabName));
+
+        
+    }
+}
