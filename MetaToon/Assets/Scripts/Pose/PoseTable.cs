371fd9554e12ae2a3274bd5af669da139ebc5cb5
Joogba <tjdwn0802@naver.com>
2023-04-06 10:51:50 +0900

    #1 스크립터블 오브젝트 추가

---
+++ MetaToon/Assets/Scripts/Pose/PoseTable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName ="PoseTable",menuName ="Scriptable Objects/PoseTable",order = int.MaxValue)]
+public class PoseTable : ScriptableObject
+{
+    public GameObject controlPrefab;
+}
--- MetaToon/Assets/Scripts/production/scrollScript.cs
+++ MetaToon/Assets/Scripts/production/scrollScript.cs
@@ -55,6 +55,9 @@ public class scrollScript : MonoBehaviour
         else if (buttonName.Equals("character"))
         {
             scroll_character.SetActive(true);
+            //temp
+            scroll_character.AddComponent<SA.FullBodyIKBehaviour>();
+            scroll_character.AddComponent<AttachControlTarget>();
         }
         else if (buttonName.Equals("pose"))
         {
