000a4d5b5eae22240203f1da2f601f0c5f3e765c
eunjin3481 <eunjin3481@gmail.com>
2023-04-14 22:02:01 +0900

    표정 스크립트 수정

--- MetaToon/Assets/Scripts/production/lookScript.cs
+++ MetaToon/Assets/Scripts/production/lookScript.cs
@@ -20,9 +20,16 @@ public class lookScript : MonoBehaviour
 
     public void LookAply()
     {
+        // 클릭한 버튼 이름 추출
         string lookName = GameObject.Find("GameObject").GetComponent<SceneChanger>().ButtonName(1);
+
+        // 아바타에 애니메이터 컨트롤러 추가
         Animator animator = avatar.GetComponent<Animator>();
-        animator.runtimeAnimatorController = (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load(lookName, typeof(RuntimeAnimatorController)));
+        animator.runtimeAnimatorController = (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load("lookAnimator", typeof(RuntimeAnimatorController)));
+        
+        // 애니메이션 클립 변경
+        animator.Play(lookName);
+
 
         // 만약 캐릭터 선택 전에 표정 선택시 알림 메시지 
     }
