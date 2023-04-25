f7c8f2995aefcc3dfb3f24c1f52ef3af3805e5be
eunjin3481 <eunjin3481@gmail.com>
2023-04-25 16:54:54 +0900

    오브젝트 사이즈조절 수정

---
+++ MetaToon/Assets/Scripts/production/ObjectSize.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ObjectSize : MonoBehaviour
+{
+    public GameObject targetObject;
+    public Slider slider_x;
+    public Slider slider_y;
+    public Slider slider_z;
+    float slider_value;
+    public GameObject size_ui;
+
+
+    public void sizeXControl()
+    {
+        if (targetObject.transform.localScale.x >= 0)
+        {
+            if (slider_x.value > slider_value)
+            {
+                targetObject.transform.localScale += new Vector3(0.5f, 0, 0);
+            }
+            else if (slider_x.value < slider_value)
+            {
+                if (targetObject.transform.localScale.x != 0)
+                {
+                    targetObject.transform.localScale -= new Vector3(0.5f, 0, 0);
+                }
+            }
+        }
+        slider_value = slider_x.value;
+    }
+
+    public void sizeYControl()
+    {
+        if (targetObject.transform.localScale.y >= 0)
+        {
+            if (slider_y.value > slider_value)
+            {
+                targetObject.transform.localScale += new Vector3(0, 0.5f, 0);
+            }
+            else if (slider_y.value < slider_value)
+            {
+                if (targetObject.transform.localScale.y != 0)
+                {
+                    targetObject.transform.localScale -= new Vector3(0, 0.5f, 0);
+                }
+            }
+        }
+        slider_value = slider_y.value;
+    }
+
+    public void sizeZControl()
+    {
+        if (targetObject.transform.localScale.z >= 0)
+        {
+            if (slider_z.value > slider_value)
+            {
+                targetObject.transform.localScale += new Vector3(0, 0, 0.5f);
+            }
+            else if (slider_z.value < slider_value)
+            {
+                if (targetObject.transform.localScale.z != 0)
+                {
+                    targetObject.transform.localScale -= new Vector3(0, 0, 0.5f);
+                }
+            }
+        }
+        slider_value = slider_z.value;
+    }
+
+
+    public void sizeControlOff()
+    {
+        slider_x.value = 5.0f;
+        slider_y.value = 5.0f;
+        slider_z.value = 5.0f;
+    }
+
+    public void sizeUiActive()
+    {
+        size_ui.SetActive(true);
+    }
+
+    public void sizeUiInActive()
+    {
+        size_ui.SetActive(false);
+    }
+
+}
+
+
+
--- MetaToon/Assets/Scripts/production/objectClickScript.cs
+++ MetaToon/Assets/Scripts/production/objectClickScript.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class objectClickScript : MonoBehaviour
 {
-    public GameObject interface_;
-
-    float distance = 10;
-
-    void start()
-    {
-        
-    }
-
-    void update()
-    {
-    
-    }
 
     void OnMouseDrag()
     {
@@ -25,31 +12,15 @@ public class objectClickScript : MonoBehaviour
 
     void OnMouseDown()
     {
-
-        //interface_ = GameObject.Find("Canvas").transform.Find("interface").gameObject;
-
-        //Invoke("InterfaceActive", 0.2f);
-
         if (gameObject.CompareTag("avatar"))
         {
             GameObject.Find("GameObject").GetComponent<lookScript>().avatar = gameObject;
         }
-        GameObject.Find("GameObject").GetComponent<objectControl>().objectName = gameObject;
+        GameObject.Find("GameObject").GetComponent<ObjectSize>().targetObject = gameObject;
         GameObject.Find("GameObject").GetComponent<ObjectMove>().targetObject = gameObject;
 
     }
     
-    void InterfaceActive()
-    {
-        interface_.SetActive(true);
-    }
-
-
-    public void InterfaceInActive()
-    {
-        interface_.SetActive(false);
-    }
-
 }
 
 
