78a9d99716644b4f0ce1b9448a0a129c9d840ff4
WooSeungMoon <smwoo529@gmail.com>
2023-04-27 17:24:18 +0900

    웹툰 뷰어 시스템 업데이트
    데모용 업데이트

---
+++ js_test/js/image_demo.js
@@ -0,0 +1,23 @@
+fetch("http://34.145.65.5:46351/file/11", {
+  headers: {
+    Accept: "image/png",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.blob(); // if response is ok, get blob data
+    }
+    throw new Error("Network response was not ok");
+  })
+  .then((data) => {
+    const imageUrl = URL.createObjectURL(data); // create an object URL from the blob data
+    const img = document.createElement("img"); // create an img element
+    img.src = imageUrl; // set the img source to the object URL
+    // img.width = 600;
+    // img.height = 3000;
+
+    // Add the image element to the image container
+    const imageContainer = document.getElementById("image-container");
+    imageContainer.appendChild(img);
+  })
+  .catch((error) => console.error("Error fetching image:", error));
