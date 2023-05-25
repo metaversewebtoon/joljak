6308e8e83a953ffab77f976d85fdbbf7d5ed72b9
WooSeungMoon <smwoo529@gmail.com>
2023-05-25 17:40:08 +0900

    zip 파일 코드 추가

--- js_test/js/test.js
+++ js_test/js/test.js
@@ -1,55 +1,34 @@
-// fetch("http://34.145.65.5:46351/file")
-//   .then((response) => {
-//     if (response.ok) {
-//       return response.blob(); // if response is ok, get blob data
-//     }
-//     throw new Error("Network response was not ok");
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     data.forEach((image) => {
-//       const imageUrl = URL.createObjectURL(image); // create an object URL from the blob data
-//       const img = document.createElement("img"); // create an img element
-//       img.src = imageUrl; // set the img source to the object URL
+/*
+html에 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js"></script>
+반드시 추가!!
+*/
 
-//       // Add the image element to the image container
-//       const imageContainer = document.getElementById("image-container");
-//       imageContainer.appendChild(img);
-//     });
-//   })
-//   .catch((error) => console.error("Error fetching :", error));
-import JSZip from "./jszip";
-
-fetch("http://34.145.65.5:46351/file")
-  .then((response) => {
-    if (response.ok) {
-      return response.arrayBuffer(); // get binary data
-    }
-    throw new Error("Network response was not ok");
+fetch("http://34.145.65.5:46351/file_archive")
+  .then(function (response) {
+    return response.blob();
   })
-  .then((data) => {
-    return JSZip.loadAsync(data); // load the binary data as a ZIP file
+  .then(function (blob) {
+    return JSZip.loadAsync(blob);
   })
-  .then((zip) => {
-    const imageNames = Object.keys(zip.files).filter((name) =>
-      name.match(/\.(jpg|jpeg|png|gif)$/i)
-    );
+  .then(function (zip) {
+    var promises = [];
 
-    return Promise.all(
-      imageNames.map((name) => {
-        return zip.file(name).async("blob"); // extract each image as a blob
-      })
-    );
-  })
-  .then((blobs) => {
-    blobs.forEach((blob) => {
-      const imageUrl = URL.createObjectURL(blob); // create an object URL from the blob data
-      const img = document.createElement("img"); // create an img element
-      img.src = imageUrl; // set the img source to the object URL
+    zip.forEach(function (relativePath, zipEntry) {
+      if (!zipEntry.dir && zipEntry.name.endsWith(".png")) {
+        var promise = zipEntry.async("blob").then(function (content) {
+          // Create an image element
+          var img = document.createElement("img");
+          img.src = URL.createObjectURL(content);
+          document.body.appendChild(img); // Append image to the body or any other container
+        });
 
-      // Add the image element to the image container
-      const imageContainer = document.getElementById("image-container");
-      imageContainer.appendChild(img);
+        promises.push(promise);
+      }
     });
+
+    return Promise.all(promises);
   })
-  .catch((error) => console.error("Error fetching:", error));
+  .catch(function (error) {
+    console.error(error);
+  });
