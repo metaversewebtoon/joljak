f1adecd85978f0e2be8f4cf2e314774679cf1122
Joogba <tjdwn0802@naver.com>
2023-06-22 12:25:46 +0900

    #6 컷 크기 조절
    -상태 enum 추가
    -왼쪽아래로 : 작게 오른쪽위로 : 크게

--- MetaToon/Assets/Scripts/StoryBoard/Cut.cs
+++ MetaToon/Assets/Scripts/StoryBoard/Cut.cs
@@ -6,9 +6,12 @@ using UnityEngine.EventSystems;
 public class Cut : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
 
-	RectTransform rectTransform;
-	CanvasGroup canvasGroup;
+    RectTransform rectTransform;
+    CanvasGroup canvasGroup;
+    KeyState keystate;
+    float scaleWeight;
 
+    enum KeyState  { scale, position };
     public Vector2 pos
 	{
 		get
@@ -26,35 +29,67 @@ public class Cut : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 	{
 		rectTransform = GetComponent<RectTransform>();
 		canvasGroup = GetComponent<CanvasGroup>();
-	}
+        scaleWeight = 0.0001f;
+        keystate = KeyState.position;
+    }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+            keystate = KeyState.scale;
+        if (Input.GetKeyUp(KeyCode.Space))
+            keystate = KeyState.position;
     }
 
     public void init(uint cutNum)
 	{
         //this.enabled = false;
         transform.position = new Vector2(0.0f, -500.0f * cutNum);
-	}
+        keystate = KeyState.position;
+
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        canvasGroup.alpha = .6f;
-        canvasGroup.blocksRaycasts = false;
+        switch(keystate)
+		{
+            case KeyState.position:
+            case KeyState.scale:
+                canvasGroup.alpha = .6f;
+                canvasGroup.blocksRaycasts = false;
+                break;
+        }
+        
     }
 
 
 
     public void OnDrag(PointerEventData eventData)
     {
+        switch (keystate)
+        {
+            case KeyState.position:
+                rectTransform.anchoredPosition += eventData.delta;
+                break;
+            case KeyState.scale:
+                rectTransform.sizeDelta  += (eventData.delta);
+                break;
+
+        }
         // 이전 이동과 비교해서 얼마나 이동했는지를 보여줌
         // 캔버스의 스케일과 맞춰야 하기 때문에
-        rectTransform.anchoredPosition += eventData.delta;
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.alpha = 1f;
-        canvasGroup.blocksRaycasts = true;
+        switch (keystate)
+        {
+            case KeyState.position:
+            case KeyState.scale:
+                canvasGroup.alpha = 1f;
+                canvasGroup.blocksRaycasts = true;
+                break;
+        }
+        
     }
 
 	public void OnPointerClick(PointerEventData eventData)
