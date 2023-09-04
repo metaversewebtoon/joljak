9d44847f38a001c01e6c34794e16b12a40e1f4f5
Joogba <tjdwn0802@naver.com>
2023-09-05 01:38:08 +0900

    터레인 패키지추가 / 오브젝트 생성시 바닥 클릭한 위치에 생성

--- MetaToon/Assets/Scripts/production/prefabScript.cs
+++ MetaToon/Assets/Scripts/production/prefabScript.cs
@@ -1,34 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class prefabScript : MonoBehaviour
 {
+
+    private bool createmode;
+
+    private string buttonName;
+    private string prefabName;
+
     // Start is called before the first frame update
     void Start()
     {
+        createmode = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Ray가 충돌한 물체를 인식한 경우 오브젝트 조정
+            if (Physics.Raycast(ray, out hit) && createmode )
+            {
+                if(hit.transform.name.Contains("Terrain"))
+				{
+                    GameObject obj = Instantiate(Resources.Load<GameObject>(prefabName));
+                    obj.transform.position = hit.point;
+                    Debug.Log("벼튼 이름" + buttonName);
+                    if (buttonName.Contains("avatar"))
+                    {
+                        obj.AddComponent<SA.FullBodyIKBehaviour>();
+                        obj.AddComponent<AttachControlTarget>();
+
+                    }
+                    createmode = false;
+                }
+                
+    
+            }
+        }
     }
 
     public void prefabCreate()
     {
-        string buttonName = GameObject.Find("GameObject").GetComponent<SceneChanger>().ButtonName(1);
-        string prefabName = buttonName + "_prefab";
+        createmode = true;
 
-        GameObject obj = Instantiate(Resources.Load<GameObject>(prefabName));
-
-        Debug.Log("벼튼 이름" + buttonName);
-        if (buttonName.Contains("avatar"))
-        {
-            obj.AddComponent<SA.FullBodyIKBehaviour>();
-            obj.AddComponent<AttachControlTarget>();
-
-        }
+        buttonName = GameObject.Find("GameObject").GetComponent<SceneChanger>().ButtonName(1);
+        prefabName = buttonName + "_prefab";
     }
 }
